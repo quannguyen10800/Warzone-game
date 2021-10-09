2d356cd32a3d3adda45b6bdb638344e588bdd5f8
quannguyen100800 <Vipervip11!>
2021-10-08 20:34:05 -0400

    Restructure project to make it runnable.

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -50,6 +50,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/PlayerDriver.dir/DependInfo.cmake"
   "CMakeFiles/OrdersDriver.dir/DependInfo.cmake"
-  "CMakeFiles/assignment1.dir/DependInfo.cmake"
+  "CMakeFiles/AssignmentLib.dir/DependInfo.cmake"
   )
