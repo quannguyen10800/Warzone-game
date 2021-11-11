e27126c212c93d0a22107ae884ae9a1f4fe19c62
mehrsaYaz <74255701+mehrsaYaz@users.noreply.github.com>
2021-11-11 18:30:02 -0500

    Add files via upload

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -1,7 +1,3 @@
-//
-// Created by mehrsa yazdani on 2021-11-07.
-//
-
 
 #ifndef ASSIGNMENT1_GAMEENGINE_H
 #define ASSIGNMENT1_GAMEENGINE_H
@@ -13,26 +9,6 @@
 #include "../Player/Player.h"
 #include <cstdlib>
 #include <ctime>
-
-//************************
-//ADDED BY MMYA - BEGIN
-//************************
-
-
-
-string CommandProcessor::getCommand() {
-    return command;
-}
-
-
-//************************
-//ADDED BY MMYA - END
-//************************
-
-
-
-
-
 
 
 GameEngine::GameEngine(){
@@ -235,10 +211,108 @@ istream& operator >> (istream &in,  GameEngine &ge){
 
 
 
+
+
+
+
 GameEngine::~GameEngine(){
     delete[] state;
 }
 
+void GameEngine::startupPhase() {
+    Map* map =new Map();
+    string nameofthemap;
+    cout << "please enter the name of the map"<<endl;
+    cin >> nameofthemap;
+    map=MapLoader::parse(nameofthemap,map);
+
+    if(!Map::validate(map)){
+        cout << "the map is validate to be incorrect , please start over" <<endl;
+        exit(0);
+
+
+    }else{
+        int numOfPlayers;
+
+        cout << "please enter the number of players " <<endl;
+        cin >> numOfPlayers;
+        while(numOfPlayers > 6  || numOfPlayers <2){
+            cout <<"the number entered is incorrect please try again" <<endl;
+            cin >> numOfPlayers;
+        }
+
+        for (int i = 0; i < numOfPlayers ; i++) {
+            string name;
+            cout << "please enter the name of the player "<<i<<endl;
+            cin  >> name;
+            Player* p1=new Player(name);
+            addPlayersToList(p1);
+        }
+        int numberofterritoris=map->territories.size();
+        //int numberofterritoriesforeachplayer;
+        int helper=numberofterritoris/numOfPlayers;
+        int helper3=helper*4;
+        int helper2=numberofterritoris-(helper3);
+
+        for (int i = 0; i < players_.size(); i++) {
+            for (int j = 0; j < helper; j++) {
+                static int indexofterritories=0;
+                players_.at(i)->addTerritory(map->territories.at(indexofterritories));
+                indexofterritories++;
+
+            }
+
+        }
+
+        for (int i = 0; i < helper2 ; i++) {
+            for (int j = 0; j < map->territories.size(); j++) {
+
+                players_.at(i)->addTerritory(map->territories.at(helper3));
+                helper3++;
+
+
+            }
+
+        }
+
+        bool order;
+        string StringStateOfTheGame="starting";
+        int turn;
+        turn =getTheTurn(numOfPlayers);
+        cout << "it is the turn for player number " <<turn <<endl;
+
+
+        for (int i = 0; i <numOfPlayers ; i++) {
+            players_.at(i)->setTheNumberOFArmies(50);
+
+        }
+
+        for (int i = 0; i <numOfPlayers ; i++) {
+            for (int j = 0; j < 2; j++) {
+                players_.at(i).draw();
+
+            }
+
+        }
+        StringStateOfTheGame="play";
+
+
+
+    }
+
+
+
+
+}
+
+int  GameEngine :: getTheTurn(int numb){
+    srand(time(0));
+    int turn =(rand() % numb + 1);
+    return turn;
+
+
+
+}
 
 
 #endif //ASSIGNMENT1_GAMEENGINE_H
