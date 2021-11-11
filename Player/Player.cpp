f4bf1aba9279b19b4892e277df6433ce057906e0
mehrsaYaz <74255701+mehrsaYaz@users.noreply.github.com>
2021-11-11 15:39:29 -0500

    Update Player.cpp

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -1,65 +1,59 @@
 //
-// Created by Mehrsa Yazdani on 2021-10-03.
+// Created by mehrsa yazdani on 2021-11-07.
 //
 
+#include "Plater.h"
+#include "Map.h"
+#include "Map.cpp"
 #include <iostream>
 #include "string"
 #include "list"
-#include "../Player/Player.h"
-#include "../Map/Map.h"
-
 //initializing  the number to zerp
 int *Player::number = new int(0);
 
 
 
 Player ::Player() {
+
     this->territories = vector<Territory *>();
     this->orders = new OrdersList();
-   // this->hand = new Hand();
-
+    this->handofcard = new Hand();
     *number=*number+1;
     this->Pname="";
     //function
-    // this->initial;
-    territories.push_back(initial);
+
+
 
 }
 Player ::Player(string s) {
     this->territories = vector<Territory *>();
     this->orders = new OrdersList();
-  //  this->hand = new Hand();
-
+    this->handofcard = new Hand();
     *number=*number+1;
     this->Pname=s;
     //function
-   // this->initial;
-    territories.push_back(initial);
 
 }
-//
-//Player ::Player(string s,vector<Territory*> territories, HandsofCards *handofcard,OrdersList *orders) {
-//
-//    this->territories = territories;
-//    this->orders = orders;
-//    this->hand = handofcard;
-//
-//    *number=*number+1;
-//    this->Pname=s;
-//    //function
-//    this->initial;
-//    territories.push_back(initial);
-//
-//}
+
+Player  ::Player(string s,vector<Territory*> territories,Hand *handofcard,OrdersList *orders) {
+
+    this->territories = territories;
+    this->orders = orders;
+    this->handofcard = handofcard;
+    *number=*number+1;
+    this->Pname=s;
+    //functio
+
+}
 //copy constructor
 Player::Player(const Player &p){
     this->territories = p.territories;
-    for (int i = 0; i < territories.size(); i++){
+    for (int i = 0; i < p.territories.size(); i++){
         this->territories.at(i) = p.territories.at(i);
     }
-    this->orders = p.orders;
-   // this->hand = p.hand;
-    this->Pname = p.Pname;
+    orders =p.orders;
+    handofcard = p.handofcard;
+    Pname = p.Pname;
 
 
 }
@@ -67,8 +61,11 @@ Player::Player(const Player &p){
 Player& Player::operator=(const Player& p) {
 
     this->territories = p.territories;
+    for (int i = 0; i < p.territories.size(); i++){
+        this->territories.at(i) = p.territories.at(i);
+    }
     this->orders = p.orders;
-   // this->hand = p.hand;
+    this->handofcard = p.handofcard;
 
     return *this;
 
@@ -78,22 +75,20 @@ Player& Player::operator=(const Player& p) {
 Player ::~Player() {
 
     delete orders;
-   // delete hand;
+    delete handofcard;
 
     for (int i = 0; i < territories.size(); i++) {
         delete territories.at(i);
 
     }
     for (int i = 0; i < toDefend.size(); i++) {
-     //   delete toDefend.front()
+        delete toDefend.front()
 
     }
     for (int i = 0; i < toAttact.size(); i++) {
         delete toAttact.front();
 
     }
-
-
 }
 
 //setname
@@ -131,7 +126,7 @@ void Player ::addTerritory(Territory *territory) {
 //remove a territories from the  territories
 void Player ::removeTerritory(Territory *territory) {
     for (int i = 0; i < territories.size(); i++) {
-        if(territories.at(i)->getName() == territory->getName()){
+        if(territories.at(i)->get_name() == territory->get_name()){
             territories.erase(territories.begin()+i);
 
         }
@@ -140,44 +135,58 @@ void Player ::removeTerritory(Territory *territory) {
 }
 
 
-list<Territory*> Player ::to_Attact() { //TODO
-    list<Territory*> aux;
-    for (int i = 0; i < territories.size(); i++) {
-        list<Territory*> aux;
-        aux=territories.at(i)->neighbours();
+vector<Territory*> Player ::to_Attact() {
+    vector<Territory*> aux;
 
-    }
-    list<Territory*>::iterator it = aux.begin();
-    for (int i=0;i<aux.size();i++){
-        for (int j=0;j<territories.size();j++){
-            if(advance(it,i).getName() == territories.at(j)->getName()){
-             break;
-            }else
-            {
-                toAttact.push_back(advance(it,i));
-            }
+    for (int i = 0; i < territories.size(); i++) {
+        for (int j = 0; j < territories.at(i)->get_bordering_territory().size(); j++) {
+            aux.push_back(territories.at(i)->get_bordering_territory().at(j));
+
+
         }
-    }
-
-
-
-}
-
-list<Territory*> Player ::to_Defend() {   //TODO
-    list<Territory*> aux;
-    for (int i = 0; i < territories.size(); i++) {
-        list<Territory*> aux;
-        aux=territories.at(i)->neighbours();
 
     }
-    list<Territory*>::iterator it = aux.begin();
+
+
     for (int i=0;i<aux.size();i++){
         for (int j=0;j<territories.size();j++){
-            if(advance(it,i).getName() == territories.at(j)->getName()){
+            if(aux.at(i)->get_name() == territories.at(j)->get_name()){
                 break;
             }else
             {
-                toDefend.push_back(advance(it,i));
+                toDefend.push_back(aux.at(i));
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
+vector<Territory*> Player ::to_Defend() {
+    vector<Territory*> aux;
+
+    for (int i = 0; i < territories.size(); i++) {
+        for (int j = 0; j < territories.at(i)->get_bordering_territory().size(); j++) {
+            aux.push_back(territories.at(i)->get_bordering_territory().at(j));
+
+
+        }
+
+    }
+
+    for (int i=0;i<aux.size();i++){
+        for (int j=0;j<territories.size();j++){
+            if(aux.at(i)->get_name() == territories.at(j)->get_name()){
+                break;
+            }else
+            {
+                toAttact.push_back(aux.at(i));
             }
         }
     }
@@ -186,27 +195,35 @@ list<Territory*> Player ::to_Defend() {   //TODO
 
 }
 
-bool Player::isNeutral() {
-    return neutral;
-}
 
-// Add an enemy player to the list of diplomatic relations for this player
-void Player::addDiplomaticRelation(Player* player)
-{
-    diplomaticRelations_.push_back(player);
-}
+void Player ::  issueOrder(Order* ord){
 
-std::vector<Player *> Player::getRelations() const
-{
-    return diplomaticRelations_;
-}
-
-void Player ::  issueOrder(string s ,vector<Territory*> territories,HandsofCards *handofcard,int priority){     //TODO
-    Player issuer =new Player(s,territories,handofcard,orders);
-    Orders m=new Orders(issuer,priority);
-    orders->add(m);
+    orders->add(ord);
 
 }
 
 
 
+Territory * Player::random_territory(string continent_name,Map* map){
+    for(Continent *continent : map->continents){
+        if (continent->get_name() == continent_name){
+            int r = rand() % (continent->get_territories().size() -1);
+            return continent->get_territories().at(r);
+        }
+    }
+}
+
+
+void Player :: AssignFirstTerritory(Map *map){
+    int  random=rand() % map->continents.size() -1;
+    territories.push_back(random_territory(map->continents.at(random)->get_name(),map));
+
+
+}
+
+int Player::getTheNumberOFArmies() {
+    return numberofarmies;
+}
+void Player::setTheNumberOFArmies(int num) {
+    numberofarmies=num;
+}
